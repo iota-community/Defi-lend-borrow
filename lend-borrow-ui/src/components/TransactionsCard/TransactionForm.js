448f770aa68ea51bcf5ef4ee6177b3a89973617c
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-08-23 17:07:29 +0530

    Add transaction modal

--- lend-borrow-ui/src/components/TransactionsCard/TransactionForm.js
+++ lend-borrow-ui/src/components/TransactionsCard/TransactionForm.js
@@ -1,10 +1,21 @@
 import React, { useState, useContext } from "react";
 import { WalletContext } from "../../context/WalletContext";
-import { Button } from "reactstrap";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const TransactionForm = ({ activeTab }) => {
   const [value, setValue] = useState(0.0);
+  const [isTransactModalOpen, setIsTransactModalOpen] = useState(false);
+
   const { connectWallet, address } = useContext(WalletContext);
+
+  const toggleModal = () => {
+    setIsTransactModalOpen(!isTransactModalOpen);
+  };
+
+  const handleConfirmTransaction = () => {
+    transact();
+    toggleModal(); // Close the modal after confirming
+  };
 
   const transact = () => {
     // Logic to handle the transaction based on the active tab
@@ -34,15 +45,44 @@ const TransactionForm = ({ activeTab }) => {
         placeholder={`Enter ${activeTab} amount`}
         className="transact-input"
       />
+      <div className="transact-details">
+        <div className="details-title">Supply balance</div>
+        <div>0</div>
+      </div>
+      <div className="transact-details">
+        <div className="details-title">Borrow limit</div>
+        <div>$0</div>
+      </div>
+      <div className="transact-details">
+        <div className="details-title">Daily earnings</div>
+        <div>$0</div>
+      </div>
       <Button
         className="connect-btn"
         style={{ background: "rgb(45 53 73)" }}
-        onClick={() => (!address ? connectWallet() : transact())}
+        onClick={() => (!address ? connectWallet() : toggleModal())}
         color="primary"
         block
       >
         {address ? "Transact" : "Connect Wallet"}
       </Button>
+
+      <Modal isOpen={isTransactModalOpen} toggle={toggleModal} centered>
+        <ModalHeader toggle={toggleModal}>Confirm Transaction</ModalHeader>
+        <ModalBody>trnsx details</ModalBody>
+        <ModalFooter>
+          <button
+            style={{ color: "green", background: "#99d199" }}
+            onClick={handleConfirmTransaction}
+            className="connect-btn"
+          >
+            Confirm Transaction
+          </button>
+          <button className="connect-btn" onClick={toggleModal}>
+            Cancel Transaction
+          </button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 };
