3a3b615f2c0d8654338dafe2312cd75c473a2f6d
Vivek Jain <vkj.vivekkumarjain@gmail.com>
2024-08-30 08:46:05 +0530

    Update navbar alignment

--- lend-borrow-ui/src/components/NavigationBar.js
+++ lend-borrow-ui/src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import ConnectWallet from "./ConnectWallet";
 import {
   Navbar,
@@ -11,11 +11,19 @@ import {
 } from "reactstrap";
 import { Context } from "../context/Context";
 import formatAddress from "../utils/formats";
+import { getNativeBalance } from "../utils/ethersUtils";
 
 export const NavigationBar = ({ setIsAccountsComponent }) => {
   const { address, tokenBal, bnbBal, disconnectWallet } = useContext(Context);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-
+  useEffect(() => {
+    const init = async () => {
+      const bal = await getNativeBalance();
+      setBal(bal);
+    };
+    init();
+  }, []);
+  const [bal, setBal] = useState(0);
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
   return (
@@ -25,27 +33,39 @@ export const NavigationBar = ({ setIsAccountsComponent }) => {
         <Nav className="ml-auto">
           <ConnectWallet />
           {address && (
-            <Dropdown isOpen={isDropdownOpen} toggle={toggleDropdown}>
-              <DropdownToggle
-                tag="span"
-                onClick={toggleDropdown}
-                data-toggle="dropdown"
-                aria-expanded={isDropdownOpen}
+            <div className="nav-right">
+              <div
                 className="accountDropdown"
+                style={{
+                  backgroundColor: "white",
+                  color: "rgb(30 36 49)",
+                  height: "37px",
+                  display: "flex",
+                  alignItems: "center",
+                  cursor: "pointer",
+                }}
+                onClick={() => setIsAccountsComponent(true)}
               >
-                Address: {formatAddress(address)} ↓
-              </DropdownToggle>
-              <DropdownMenu end>
-                <DropdownItem header>Wallet Details</DropdownItem>
-                <DropdownItem onClick={() => setIsAccountsComponent(true)}>
-                  Go to Accounts Section{" "}
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem onClick={disconnectWallet}>
-                  Disconnect Wallet
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+                Go to Accounts Section
+              </div>
+              <Dropdown isOpen={isDropdownOpen} toggle={toggleDropdown}>
+                <DropdownToggle
+                  tag="span"
+                  onClick={toggleDropdown}
+                  data-toggle="dropdown"
+                  aria-expanded={isDropdownOpen}
+                  className="accountDropdown"
+                >
+                  Address: {formatAddress(address)}
+                </DropdownToggle>
+                <DropdownMenu end>
+                  <DropdownItem>Balance: {bal} </DropdownItem>
+                  <DropdownItem onClick={disconnectWallet}>
+                    Disconnect Wallet
+                  </DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
+            </div>
           )}
         </Nav>
       </Navbar>
